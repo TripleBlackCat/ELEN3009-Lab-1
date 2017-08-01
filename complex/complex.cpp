fd6e44ad480549bfb90452a5a722d932b2c4fb89
Kavilan Nair <kavilann@gmail.com>
2017-08-01 22:12:06 +0200

    Exercise 3.2

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -5,11 +5,12 @@
 #include <complex> // required for the complex class
 
 using namespace std;
+using complexNumber = complex<float>;
 
 int main()
 {
-    complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
-    complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+    complexNumber num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
+    complexNumber num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
 
     auto answer = num1 * num2; // using C++11 auto keyword,
                                // answer is of type: complex<float>
