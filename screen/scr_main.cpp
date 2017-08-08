a6e263f8d05904acb46d89426f6808fd2a0ccc79
Kavilan Nair <kavilann@gmail.com>
2017-08-08 12:33:02 +0200

    Exercise 4.6

--- screen/scr_main.cpp
+++ screen/scr_main.cpp
@@ -8,12 +8,14 @@ int main()
 {
 	Screen myScreen{9,9};
 	
-	int x = 3;
-	int y = 3;
-	int sideLength = 3;
+	int x = 8;
+	int y = 8;
+	int sideLength = 5;
 	
 	myScreen.drawSquare(x, y, sideLength);
 	myScreen.display();
 
+	// Changing the class's interface can be catastrophic as it could change the expected outputs and inputs. It is preferable to only change the implementation. As changing the implementation doesnt change the expected inputs and outputs for functions.
+	
 	return 0;
 }
